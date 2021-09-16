396420b53ad4d4c5b6e09a9f4616d51297d23e32
statue <sethschmidt93@gmail.com>
2021-09-16 13:03:35 -0400

    test2

--- jstest.js
+++ jstest.js
@@ -1,6 +1,5 @@
 var Web3 = require('web3');
 var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
-var web4 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 web3.eth.getAccounts()
 .then(console.log);
 
